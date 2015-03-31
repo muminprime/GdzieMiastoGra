282e84da58ffe4f617b946020e23e5c1065ae83c
e-konrad-wlazlo <konrad1kon@op.pl>
2015-03-31 15:39:36 +0200

    Aplikacja v.1.0
    Próba łączenia się z mySQL

---
+++ Aplikacja/index.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+
+<script type="text/javascript" src="jquery.js"></script>
+
+<script type="text/javascript">
+
+	function get(){
+		$.post('data.php', {name: form.name.value}, 
+			function(output) {
+				$('#age').html(output).show();
+			});
+	}
+
+</script>
+
+</head>
+
+<body>
+
+<p>
+<form name="form">
+	<input type="text" name="name"><input type="button" value="Get" onClick="get();">
+</form>
+
+<div id="age"> </div>
+</p>
+
+<?php
+	echo "fff";
+?>
+
+</body>
+</html>
