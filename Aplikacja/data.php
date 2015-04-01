c3955a1181300e4ff70d9d047f6d037aa51b93a7
muminprime <matsolyga@wp.pl>
2015-04-01 20:18:12 +0200

    wczytywanie koncertow z bazy

--- Aplikacja/data.php
+++ Aplikacja/data.php
@@ -3,18 +3,44 @@
 	#$name = mysql_real_escape_string($_POST['name']);
 	
 	
-	$wynik = mysql_query("SELECT * FROM database.people ");
+	$wynik = mysql_query("SELECT * FROM database.koncerty ");
 	$wynik_num_rows = mysql_num_rows($wynik);
 		
 	if($wynik_num_rows==0)
-		echo "Name does not exist!";
+		echo "Nie ma zadnych koncertow";
 	else
 	{
-			
 		while($data=mysql_fetch_assoc($wynik)){
-		$dohtml="<center>".$data['name']." ".$data['Nazwisko']." age is ".$data['age']."</center>";
-		$dohtml.="<center>and his index is ".$data['indeks']."</center><p></p>";
-		echo $dohtml;	
+		echo "<div id=\"koncert\">
+		<div id=\"koncert_logo\">
+		<p style=\"margin-top:40%;margin-left:40%\" >Logo</p>
+		</div>
+		<div id=\"koncert_nazwa\">
+		<b>
+		<p style=\"margin-left:45%;margin-top:0.5%;\">".$data['nazwa_zespolu']."</p>
+		</b>
+		</div>
+		<div id=\"koncert_lewe_dane\">
+		<b>
+		<p style=\"margin-top:3%;margin-left:3%\">Gatunek muzyki: ".$data['gatunek']."</p>
+		<p style=\"margin-top:3%;margin-left:3%\">Gdzie grają: ".$data['nazwa_lokalu']."</p>
+		<p style=\"margin-top:3%;margin-left:3%\">Adres: ".$data['adres_lokalu']."</p>
+		</b>
+		</div>
+		<div id=\"koncert_prawe_dane\">
+		<b>
+		<p style=\"margin-top:3%;margin-left:3%\">Data i godzina: ".$data['data_godzina']."</p>
+		<p style=\"margin-top:3%;margin-left:3%\">Cena biletu: ".$data['cena']."</p>
+		<p style=\"margin-top:3%;margin-left:3%\">Ograniczenia wiekowe: ".$data['wiek']."</p>
+		</b>
+		</div>	
+		</div>";
+		
+		
+		
+		#$dohtml="<center>".$data['name']." ".$data['Nazwisko']." age is ".$data['age']."</center>";
+		#$dohtml.="<center>and his index is ".$data['indeks']."</center><p></p>";
+		#echo $dohtml;	
 		}
 			
 	}
