0c66b38a544c562e811c2e041c48596fc9931b89
muminprime <matsolyga@wp.pl>
2015-04-01 19:00:29 +0200

    dodanie plik main.css
    zmiany obejmuja również przebudwanie plików index.php oraz data.php w
    kierunku naszej aplikacji aby wszystko działao trzeba mieć odpowiednią
    bazę

--- Aplikacja/data.php
+++ Aplikacja/data.php
@@ -1,20 +1,22 @@
 <?php
 	mysql_connect("localhost","root","");
-	$name = mysql_real_escape_string($_POST['name']);
+	#$name = mysql_real_escape_string($_POST['name']);
 	
-	if($name==NULL)
-		echo "Please enter a name!";
+	
+	$wynik = mysql_query("SELECT * FROM database.people ");
+	$wynik_num_rows = mysql_num_rows($wynik);
+		
+	if($wynik_num_rows==0)
+		echo "Name does not exist!";
 	else
 	{
-		$age = mysql_query("SELECT age FROM database.people WHERE name='$name'");
-		$age_num_rows = mysql_num_rows($age);
-		
-		if($age_num_rows==0)
-			echo "Name does not exist!";
-		else
-		{
-			$age = mysql_result($age,0);
-			echo "$name's age is $age";
+			
+		while($data=mysql_fetch_assoc($wynik)){
+		$dohtml="<center>".$data['name']." ".$data['Nazwisko']." age is ".$data['age']."</center>";
+		$dohtml.="<center>and his index is ".$data['indeks']."</center><p></p>";
+		echo $dohtml;	
 		}
+			
 	}
+	
 ?>
--- Aplikacja/index.php
+++ Aplikacja/index.php
@@ -1,14 +1,15 @@
 <html>
 <head>
 
+<link rel="Stylesheet" type="text/css" href="style/main.css" />
 <script type="text/javascript" src="jquery.js"></script>
 
 <script type="text/javascript">
 
 	function get(){
-		$.post('data.php', {name: form.name.value}, 
+		$.post('data.php', {}, 
 			function(output) {
-				$('#age').html(output).show();
+				$('#scena').html(output).show();
 			});
 	}
 
@@ -18,13 +19,142 @@
 
 <body>
 
-<p>
-<form name="form">
-	<input type="text" name="name"><input type="button" value="Get" onClick="get();">
-</form>
+	<center><input type="button" value="Pokaż koncerty" onClick="get();"/></center>
+	<p></p>
 
-<div id="age"> </div>
-</p>
+
+
+
+<div id="scena">
+
+	<div id="koncert">
+		<div id="koncert_logo">
+		<p style="margin-top:40%;margin-left:40%" >Logo</p>
+		</div>
+		<div id="koncert_nazwa">
+		<p style="margin-left:45%;margin-top:5px;">Nazwa zespołu</p>
+		</div>
+		<div id="koncert_lewe_dane">
+		<p style="margin-top:10px;margin-left:10px">Gatunek muzyki: jazz</p>
+		<p style="margin-top:10px;margin-left:10px">Gdzie grają: gdzieś</p>
+		<p style="margin-top:10px;margin-left:10px">Adres:Radwańska 73</p>
+		</div>
+		<div id="koncert_prawe_dane">
+		<p style="margin-top:10px;margin-left:10px">Data i godzina:22.12.2016 22:00</p>
+		<p style="margin-top:10px;margin-left:10px">Cena biletu: 10zł</p>
+		<p style="margin-top:10px;margin-left:10px">Ograniczenia wiekowe:brak</p>
+		</div>	
+	</div>
+	
+	<div id="koncert">
+		<div id="koncert_logo">
+		<p style="margin-top:40%;margin-left:40%" >Logo</p>
+		</div>
+		<div id="koncert_nazwa">
+		<p style="margin-left:45%;margin-top:5px;">Nazwa zespołu</p>
+		</div>
+		<div id="koncert_lewe_dane">
+		<p style="margin-top:10px;margin-left:10px">Gatunek muzyki: jazz</p>
+		<p style="margin-top:10px;margin-left:10px">Gdzie grają: gdzieś</p>
+		<p style="margin-top:10px;margin-left:10px">Adres:Radwańska 73</p>
+		</div>
+		<div id="koncert_prawe_dane">
+		<p style="margin-top:10px;margin-left:10px">Data i godzina:22.12.2016 22:00</p>
+		<p style="margin-top:10px;margin-left:10px">Cena biletu: 10zł</p>
+		<p style="margin-top:10px;margin-left:10px">Ograniczenia wiekowe:brak</p>
+		</div>	
+	</div>
+	<div id="koncert">
+		<div id="koncert_logo">
+		<p style="margin-top:40%;margin-left:40%" >Logo</p>
+		</div>
+		<div id="koncert_nazwa">
+		<p style="margin-left:45%;margin-top:5px;">Nazwa zespołu</p>
+		</div>
+		<div id="koncert_lewe_dane">
+		<p style="margin-top:10px;margin-left:10px">Gatunek muzyki: jazz</p>
+		<p style="margin-top:10px;margin-left:10px">Gdzie grają: gdzieś</p>
+		<p style="margin-top:10px;margin-left:10px">Adres:Radwańska 73</p>
+		</div>
+		<div id="koncert_prawe_dane">
+		<p style="margin-top:10px;margin-left:10px">Data i godzina:22.12.2016 22:00</p>
+		<p style="margin-top:10px;margin-left:10px">Cena biletu: 10zł</p>
+		<p style="margin-top:10px;margin-left:10px">Ograniczenia wiekowe:brak</p>
+		</div>	
+	</div>
+	<div id="koncert">
+		<div id="koncert_logo">
+		<p style="margin-top:40%;margin-left:40%" >Logo</p>
+		</div>
+		<div id="koncert_nazwa">
+		<p style="margin-left:45%;margin-top:5px;">Nazwa zespołu</p>
+		</div>
+		<div id="koncert_lewe_dane">
+		<p style="margin-top:10px;margin-left:10px">Gatunek muzyki: jazz</p>
+		<p style="margin-top:10px;margin-left:10px">Gdzie grają: gdzieś</p>
+		<p style="margin-top:10px;margin-left:10px">Adres:Radwańska 73</p>
+		</div>
+		<div id="koncert_prawe_dane">
+		<p style="margin-top:10px;margin-left:10px">Data i godzina:22.12.2016 22:00</p>
+		<p style="margin-top:10px;margin-left:10px">Cena biletu: 10zł</p>
+		<p style="margin-top:10px;margin-left:10px">Ograniczenia wiekowe:brak</p>
+		</div>	
+	</div>
+	<div id="koncert">
+		<div id="koncert_logo">
+		<p style="margin-top:40%;margin-left:40%" >Logo</p>
+		</div>
+		<div id="koncert_nazwa">
+		<p style="margin-left:45%;margin-top:5px;">Nazwa zespołu</p>
+		</div>
+		<div id="koncert_lewe_dane">
+		<p style="margin-top:10px;margin-left:10px">Gatunek muzyki: jazz</p>
+		<p style="margin-top:10px;margin-left:10px">Gdzie grają: gdzieś</p>
+		<p style="margin-top:10px;margin-left:10px">Adres:Radwańska 73</p>
+		</div>
+		<div id="koncert_prawe_dane">
+		<p style="margin-top:10px;margin-left:10px">Data i godzina:22.12.2016 22:00</p>
+		<p style="margin-top:10px;margin-left:10px">Cena biletu: 10zł</p>
+		<p style="margin-top:10px;margin-left:10px">Ograniczenia wiekowe:brak</p>
+		</div>	
+	</div>
+	<div id="koncert">
+		<div id="koncert_logo">
+		<p style="margin-top:40%;margin-left:40%" >Logo</p>
+		</div>
+		<div id="koncert_nazwa">
+		<p style="margin-left:45%;margin-top:5px;">Nazwa zespołu</p>
+		</div>
+		<div id="koncert_lewe_dane">
+		<p style="margin-top:10px;margin-left:10px">Gatunek muzyki: jazz</p>
+		<p style="margin-top:10px;margin-left:10px">Gdzie grają: gdzieś</p>
+		<p style="margin-top:10px;margin-left:10px">Adres:Radwańska 73</p>
+		</div>
+		<div id="koncert_prawe_dane">
+		<p style="margin-top:10px;margin-left:10px">Data i godzina:22.12.2016 22:00</p>
+		<p style="margin-top:10px;margin-left:10px">Cena biletu: 10zł</p>
+		<p style="margin-top:10px;margin-left:10px">Ograniczenia wiekowe:brak</p>
+		</div>	
+	</div>
+	<div id="koncert">
+		<div id="koncert_logo">
+		<p style="margin-top:40%;margin-left:40%" >Logo</p>
+		</div>
+		<div id="koncert_nazwa">
+		<p style="margin-left:45%;margin-top:5px;">Nazwa zespołu</p>
+		</div>
+		<div id="koncert_lewe_dane">
+		<p style="margin-top:10px;margin-left:10px">Gatunek muzyki: jazz</p>
+		<p style="margin-top:10px;margin-left:10px">Gdzie grają: gdzieś</p>
+		<p style="margin-top:10px;margin-left:10px">Adres:Radwańska 73</p>
+		</div>
+		<div id="koncert_prawe_dane">
+		<p style="margin-top:10px;margin-left:10px">Data i godzina:22.12.2016 22:00</p>
+		<p style="margin-top:10px;margin-left:10px">Cena biletu: 10zł</p>
+		<p style="margin-top:10px;margin-left:10px">Ograniczenia wiekowe:brak</p>
+		</div>	
+	</div>
+
 
 </body>
 </html>
