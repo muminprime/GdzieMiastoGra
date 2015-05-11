7781557c79445b8915f88e0010baa065d5bee6f0
e-konrad-wlazlo <konrad1kon@op.pl>
2015-05-11 23:47:52 +0200

    Wyświetlanie kilku gatunków
    Zmodyfikowałem bazę danych i relacje. Poprawiłem index.php, żeby
    poprawnie wyświetlać dane na podstawie nowej struktury bazy danych. Do
    filtrów jeszcze tych poprawek nie wprowadziłem, więc na razie nie
    działają w ogóle.

--- Aplikacja/data.php
+++ Aplikacja/data.php
@@ -27,19 +27,68 @@
 	*/
 	
 	
+	$gatunki_zespolow = mysql_query("	SELECT 	zespoly.id as 'id',
+																						zespoly.nazwa as 'zespol', 
+																						gatunki.nazwa as 'gatunek'
+																		FROM database.zespoly, database.gatunki
+																		WHERE zespoly.gatunki_id1 = gatunki.id 
+																		OR zespoly.gatunki_id2 = gatunki.id 
+																		OR zespoly.gatunki_id3 = gatunki.id
+																		ORDER BY zespol");
+													
+	$gatunki_num_rows = mysql_num_rows($gatunki_zespolow);
+	
+	
+	/*
+		PONIŻSZY BLOK JEST DOŚĆ SKOMPLIKOWANY, ALE DZIAŁA!
+		Chodzi o to, że z tabeli 'zespoly' pobieramy sobie dane dotyczące gatunków, które w tej tabeli są reprezentowane przez numery id z tabeli 'gatunki'. 
+		Te dane zlepiamy w jednego stringa i ładujemy do specjalnego rekordu w tabeli 'zespoly'. Dzięki temu możemy prostym zapytaniem SQL wyświetlić wszystkie gatunki (praktycznie tak, jak to robiliśmy poprzednio).
+		Taki sposób wydawał mi się mimo wszystko najprostszy.
+		Trzeba mieć świadomość, że poniższy blok powinien być wykonywany za każdym razem, gdy ktoś dokona zmian w gatunkach danego zespołu, albo przy każdym wyświetlaniu wyników/filtrowaniu*
+				*czy na pewno przy filtrowaniu będę w stanie stwierdzić jak już naprawię filtrowanie
+	*/
+	$all_gatunki = "";
+	$zespol_poprzedni = mysql_fetch_assoc(mysql_query("SELECT zespoly.nazwa as 'zespol' FROM database.zespoly ORDER BY zespol"))['zespol']; // przypisujemy pierwszy zespol pod ta zmienna (order by musi byc takie samo, jak w zapytaniu $gatunki_zespolow)
+	while($data=mysql_fetch_assoc($gatunki_zespolow))
+	{
+		if ((($zespol = $data['zespol']) == $zespol_poprzedni) AND ($data['gatunek'] != "")) // jeśli ciągle rozpatrujemy ten sam zespół i jednocześnie rozpatrywane pole gatunku nie jest puste
+		{
+			$all_gatunki .= $data['gatunek'].", ";
+			$id = $data['id']; // potrzebne w elsie, zeby wzielo poprzednia wartosc id, a nie juz nastepna (bo wejscie do else'a nastepuje, gdy juz sprawdzilismy wszystkie gatunki danego zespolu i przeskoczylismy na pierwszy rekord nastepnego zespolu)
+		}
+		else
+		{
+			wpisz_gatunki_do_bazy($all_gatunki, $id);
+			$all_gatunki = ""; // wyzeruj zmienną
+			if ($data['gatunek'] != "") // wykonujemy raz to, co w ifie, pod warunkiem ze nie jest to puste pole
+				$all_gatunki .= $data['gatunek'].", ";
+			$zespol = $data['zespol']; // niezależnie od powyższego przypisujemy obecny zespół pod zmienną
+		}	
+		$zespol_poprzedni = $zespol;
+	}
+	// ponizej musimy dac te funkcje, bo powyzszy while jest tak skonstruowany, ze dla ostatniego wiersza $gatunki_zespolow nigdy nie wejdzie w else, czyli nigdy nie wykona wpisz_gatunki_do_bazy...tak wiec trzeba to zapewnic po wyjsciu z petli
+	wpisz_gatunki_do_bazy($all_gatunki, $id);
+	
+	function wpisz_gatunki_do_bazy($gat, $id)
+	{
+		$gat = trim($gat); // obcinamy stringa z ewentualnych spacji dookoła
+		$gat = trim($gat, ","); // obcinamy z przecinków
+		$zapytanie = "UPDATE database.zespoly SET gatunki='".$gat."' WHERE id='".$id."'"; //wpisujemy all_gatunki do odpowiedniej tabeli
+		mysql_query($zapytanie);
+	}
+	
 	
 	$wynik = mysql_query("SELECT 	koncerty.id as 'id koncertu', 
 																koncerty.data_godzina as 'data_godzina', 
 																zespoly.nazwa as 'nazwa_zespolu', 
 																lokale.nazwa as 'nazwa_lokalu',
-																gatunki.nazwa as 'gatunek',
+																zespoly.gatunki as 'gatunek',
 																lokale.adres as 'adres_lokalu',
 																koncerty.cena,
 																koncerty.wiek
-												FROM database.koncerty, database.zespoly, database.lokale, database.gatunki 
+												FROM database.koncerty, database.zespoly, database.lokale 
 												WHERE koncerty.zespoly_id=zespoly.id 
-												AND koncerty.lokale_id=lokale.id 
-												AND zespoly.gatunki_id=gatunki.id");
+												AND koncerty.lokale_id=lokale.id");
 	$wynik_num_rows = mysql_num_rows($wynik);
 		
 	if($wynik_num_rows==0)
--- Baza_danych/glowne_zapytanie.sql
+++ Baza_danych/glowne_zapytanie.sql
@@ -2,9 +2,9 @@ SELECT 	koncerty.id as 'id koncertu',
 		koncerty.data_godzina as 'kiedy', 
         zespoly.nazwa as 'zespol', 
         lokale.nazwa as 'lokal',
-        gatunki.nazwa as 'gatunek',
+        zespoly.gatunki as 'gatunek',
         lokale.adres as 'adres',
         koncerty.cena,
         koncerty.wiek
-FROM koncerty, zespoly, lokale, gatunki
-WHERE koncerty.zespoly_id=zespoly.id AND koncerty.lokale_id=lokale.id AND zespoly.gatunki_id=gatunki.id
+FROM koncerty, zespoly, lokale
+WHERE koncerty.zespoly_id=zespoly.id AND koncerty.lokale_id=lokale.id
